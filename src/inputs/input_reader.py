0585ad3e95fa4d8ab6beff94768721ecfd461f1b
Martyna Chojnowska <martynachojnowska12@gmail.com>
2023-10-05 13:30:59 +0200

    Text validation fixed

--- src/inputs/input_reader.py
+++ src/inputs/input_reader.py
@@ -1,14 +1,14 @@
 import re
 
-
 class InputReader:
     @staticmethod
     def get_user_input(message_to_user):
-        user_input = input(message_to_user)
-        if InputReader.text_validation(user_input):
-            return user_input
-        else:
-            print("Don't use polish signs")
+        while True:
+            user_input = input(message_to_user)
+            if InputReader.text_validation(user_input):
+                return user_input
+            else:
+                print("Don't use polish signs")
 
     @staticmethod
     def text_validation(check_this_text):
