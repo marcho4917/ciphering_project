d571b7be369bbd439aae7ef22377e9854f5b07d4
Martyna Chojnowska <martynachojnowska12@gmail.com>
2023-10-05 13:15:41 +0200

    Implementation of the main menu as a facade

---
+++ src/menus/menu.py
@@ -0,0 +1,50 @@
+from src.ciphering.rot13 import Rot13
+from src.ciphering.rot47 import Rot47
+from src.files.file_handler import FileHandler
+from src.inputs.input_reader import InputReader
+
+
+class Menu:
+    def __init__(self):
+        self.input_reader = InputReader()
+        self.rot13 = Rot13()
+        self.rot47 = Rot47()
+        self.file_handler = FileHandler()
+        self.text_dict = {}
+
+    def show_menu(self):
+        while True:
+            print("1. Encrypt plain text (ROT47/ ROT13)")
+            print("2. Save encrypted text to file")
+            print("3. Decrypt encrypted text from file")
+            print("4. Print encrypted words stored in memory")
+            print("5. Exit")
+
+            option = self.input_reader.get_user_input("Chose option: ")
+
+            if option == "1":
+                text_to_encrypt = self.input_reader.get_user_input("Write your text: ")
+                rot_type = self.input_reader.get_user_input("ROT13 or ROT47: ").lower()
+                if rot_type in ('rot13', 'rot47'):
+                    encrypted_text = getattr(self, rot_type).encrypt(text_to_encrypt)
+                # if rot_type.lower() == "rot13":
+                #     self.rot13.encrypt(text_to_encrypt)
+                # elif rot_type.lower() == "rot47":
+                #     self.rot47.encrypt(text_to_encrypt)
+                else:
+                    print("Wrong rot type!")
+                self.text_dict.update({rot_type: encrypted_text})
+            elif option == "2":
+                filename = self.input_reader.get_user_input("Give filename: ")
+                self.file_handler.save_to_file(filename, self.text_dict)
+            elif option == "3":
+                filename = self.input_reader.get_user_input("Chose file with text to encrypt: ")
+                self.file_handler.read_from_file(filename)
+            elif option == "4":
+                for k, v in self.text_dict.items():
+                    print(f"{k}: {v}")
+            elif option == "5":
+                print("Goodbye!")
+                break
+
+
