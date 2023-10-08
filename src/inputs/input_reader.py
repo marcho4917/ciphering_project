269a5b6eac2f199d193685683e4d598a41802910
Martyna Chojnowska <martynachojnowska12@gmail.com>
2023-10-08 19:32:06 +0200

    Input reader fixed, now this is more readeble for user

--- src/inputs/input_reader.py
+++ src/inputs/input_reader.py
@@ -5,12 +5,11 @@ class InputReader:
     def get_user_input(message_to_user):
         while True:
             user_input = input(message_to_user)
-            if InputReader.text_validation(user_input):
+            if not InputReader.contains_polish_characters(user_input):
                 return user_input
-            else:
-                print("Don't use Polish signs")
+            print("Don't use polish characters")
 
     @staticmethod
-    def text_validation(check_this_text):
-        pattern = re.match("[^ąĄćĆęĘłŁńŃóÓśŚżŻźŹ]+$", check_this_text)
+    def contains_polish_characters(check_this_text):
+        pattern = re.search("[ąĄćĆęĘłŁńŃóÓśŚżŻźŹ]", check_this_text)
         return pattern
