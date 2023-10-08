8cc50a2238fa427bbc1614942db2063a2f065a8a
Martyna Chojnowska <martynachojnowska12@gmail.com>
2023-10-08 23:11:51 +0200

    Setting a default value for encrypted_text, and protecting against an error as a result of calling option 2 without any encrypted text

--- src/menus/menu.py
+++ src/menus/menu.py
@@ -11,6 +11,7 @@ class Menu:
         self.rot47 = Rot47()
         self.file_handler = FileHandler()
         self.text_dict = {}
+        self.encrypted_text = None
 
     def show_menu(self):
         while True:
@@ -26,17 +27,20 @@ class Menu:
                 text_to_encrypt = self.input_reader.get_user_input("Write your text: ")
                 rot_type = self.input_reader.get_user_input("ROT13 or ROT47: ").lower()
                 if rot_type in ('rot13', 'rot47'):
-                    encrypted_text = getattr(self, rot_type).encrypt(text_to_encrypt)
+                    self.encrypted_text = getattr(self, rot_type).encrypt(text_to_encrypt)
                 # if rot_type.lower() == "rot13":
                 #     self.rot13.encrypt(text_to_encrypt)
                 # elif rot_type.lower() == "rot47":
                 #     self.rot47.encrypt(text_to_encrypt)
                 else:
                     print("Wrong rot type!")
-                self.text_dict.update({rot_type: encrypted_text})
+                self.text_dict.update({rot_type: self.encrypted_text})
             elif option == "2":
-                filename = self.input_reader.get_user_input("Give filename: ")
-                self.file_handler.save_to_file(filename, self.text_dict)
+                if self.encrypted_text is not None:
+                    filename = self.input_reader.get_user_input("Give filename: ")
+                    self.file_handler.save_to_file(filename, self.text_dict)
+                else:
+                    print("You have to encrypt text first!")
             elif option == "3":
                 filename = self.input_reader.get_user_input("Chose file with text to decrypt: ")
                 decrypt_type = self.input_reader.get_user_input("ROT13 or ROT47: ").lower()
