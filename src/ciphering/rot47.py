af46d8f257cb268f55a9f054b2071689b18130f4
Martyna Chojnowska <martynachojnowska12@gmail.com>
2023-10-09 23:06:46 +0200

    Encrypt method overrided

--- src/ciphering/rot47.py
+++ src/ciphering/rot47.py
@@ -4,3 +4,14 @@ from src.ciphering.rot import Rot
 class Rot47(Rot):
     def __init__(self):
         super().__init__(47)
+
+    def encrypt(self, text):
+        result = ""
+        for i in range(len(text)):
+            j = ord(text[i])
+            if 33 <= j <= 126:
+                result += chr(33 + (j + (self.shift - 33)) % 94)
+            else:
+                result += text[i]
+        return result
+
